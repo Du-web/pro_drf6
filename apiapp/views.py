481f651bd620a3394f82cb8f6063d564015d075c
dudu <648302938@qq.com>
2020-09-17 23:39:11 +0800

    在demo类视图的get方法中通过一个表查询另一个表

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group, Permission
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,6 +11,17 @@ from apiapp.models import User
 class Demo(APIView):
 
     def get(self, request, *args, **kwargs):
-        user = User.objects.filter()
-        print(user)
+        # user = User.objects.all().first()
+        # print(user.email)
+        # print(user.groups.first().name)
+        # print(user.user_permissions.first().name)
+
+        # group = Group.objects.first()
+        # print(group.user_set.first().username)
+        # print(group.permissions.first().name)
+
+        # permission = Permission.objects.first()
+        # print(permission.user_set.first().username)
+        # per = Permission.objects.filter(pk=1).first()
+        # print(per.group_set.first().name)
         return Response('ok')
